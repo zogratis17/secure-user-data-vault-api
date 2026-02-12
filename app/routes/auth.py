8f89c1aad506a02ecb7b3a5718a9b4df9afe53c2
HARI PRASATH N T <87246155+zogratis17@users.noreply.github.com>
2026-02-12 22:04:49 +0530

    feat: implement login endpoint with JWT authentication

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -3,6 +3,9 @@ from app.schemas.user import UserRegister, UserResponse
 from app.models.user import UserModel
 from app.database import mongodb
 from app.core.security import hash_password
+from app.schemas.user import UserLogin
+from app.core.security import verify_password, create_access_token
+
 
 router = APIRouter(prefix="/auth", tags=["Auth"])
 
@@ -37,3 +40,24 @@ def register(user: UserRegister):
         "id": str(result.inserted_id),
         "email": user.email
     }
+
+@router.post("/login")
+def login(user: UserLogin):
+    """
+    Authenticate user and return JWT token.
+    """
+
+    # Find user
+    db_user = mongodb.users.find_one({"email": user.email})
+
+    if not db_user:
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    # Verify password
+    if not verify_password(user.password, db_user["hashed_password"]):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    # Create token
+    token = create_access_token({"sub": str(db_user["_id"])})
+
+    return {"access_token": token, "token_type": "bearer"}
